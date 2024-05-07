5277d2f1c25d6e26b218300d41cca3868895a69c
csh0616 <wetopqrss597@gmail.com>
2024-05-07 14:14:49 +0900

    feat: 새로운 문서 추가

--- 과제/Ex03/Calculator.js
+++ 과제/Ex03/Calculator.js
@@ -1,8 +1,8 @@
 var firstNumberInput = document.getElementById("FirstNumber");
-var secondNumberInput = document.getElementById("SecondNumber");
+var secondNumberInput = document.getElementById("SecondNumber"); 
 var operatorInput = document.getElementById("OperatorInput"); // OperatorInput의 id를 가진 입력 필드를 가정
 
-function calculate() {
+function calculate() { 
     var firstNumber = parseFloat(firstNumberInput.value); // 입력값을 실수로 변환
     var secondNumber = parseFloat(secondNumberInput.value);
     var operator = operatorInput.value; // 연산자 입력값 가져오기
